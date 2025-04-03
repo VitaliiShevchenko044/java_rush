2a2e1117b3849a3e3d5d9a6375a60f3d44c60671
Vitalii Shevchenko <v.shevchenko044@gmail.com>
2025-04-03 06:39:06 +0300

    рівень 2 - Основи ООП: перевантаження, поліморфізм, абстракція, інтерфейси

---
+++ src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int number = 5;
+        print(number);
+        Integer num = 10;
+        print(num);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int num) {
+
+    }
+
+    public static void print(Integer num) {
+
+    }
+}
---
+++ src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int num) {}
+    public static void print(String str) {}
+    public static void print(int num1, int num2) {}
+    public static void print(String str1, String str2) {}
+    public static void print(String str, int num) {}
+}
